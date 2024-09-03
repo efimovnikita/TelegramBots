534d68a5fb55ea8b9524f6af26122edb8cdb745a
Nikita Efimov <maskedballmail@gmail.com>
2024-09-03 07:35:57 +0300

    refactoring

--- Bot.YTMusicFetcher/Services/UpdateHandler.cs
+++ Bot.YTMusicFetcher/Services/UpdateHandler.cs
@@ -15,10 +15,79 @@ using Message = Telegram.Bot.Types.Message;
 
 namespace Bot.YTMusicFetcher.Services;
 
-public class UserJob(long userId, UpdateHandler.Job job)
+public class UserJob(long userId, Job job)
 {
-    public long UserId { get; set; } = userId;
-    public UpdateHandler.Job Job { get; set; } = job;
+    public long UserId { get; } = userId;
+    public Job Job { get; } = job;
+}
+
+public class Job(string[] urls, IYtMusicApi ytMusicApi, IAuthApi authApi, IConfiguration configuration)
+{
+    private readonly YoutubeClient _youtube = new();
+
+    public string JobId { get; private set; } = string.Empty;
+    public string[] Urls { get; } = urls;
+
+    public string Status { get; private set; } = Statuses.UnknownStatus;
+    public string Result { get; private set; } = string.Empty;
+
+    public async Task<string> GetFirstTrackName()
+    {
+        var url = Urls.FirstOrDefault();
+        if (url == null)
+        {
+            return string.Empty;
+        }
+        
+        var metaData = await _youtube.Videos.GetAsync(url);
+        
+        return metaData.Title;
+    }
+    
+    public async Task<string> GetLastTrackName()
+    {
+        var url = Urls.LastOrDefault();
+        if (url == null)
+        {
+            return string.Empty;
+        }
+        
+        var metaData = await _youtube.Videos.GetAsync(url);
+        
+        return metaData.Title;
+    }
+
+    public async Task UpdateStatus()
+    {
+        var response = await ytMusicApi.CheckStatus(JobId);
+        Status = response.Status;
+        Result = response.Result;
+    }
+
+    public async Task IgniteRemoteJob()
+    {
+        await ytMusicApi.CheckHealth();
+        
+        var data = new Dictionary<string, string> {
+            { IAuthApi.GrantType, IAuthApi.ClientCredentials },
+            { IAuthApi.ClientId, configuration["BotConfiguration:ClientId"] ?? "" },
+            { IAuthApi.ClientSecret, configuration["BotConfiguration:ClientSecret"] ?? "" }
+        };
+
+        var authData = await authApi.GetAuthData(data);
+        
+        var job = await ytMusicApi.StartMakingArchiveJob($"Bearer {authData.AccessToken}",
+            new UrlsRequest { Urls = Urls});
+        
+        JobId = job.JobId;
+    }
+}
+
+public static class Statuses
+{
+    public const string SuccessStatus = "Succeeded";
+    public const string FailedStatus = "Failed";
+    public const string UnknownStatus = "Unknown";
 }
 
 public class UpdateHandler(
@@ -31,8 +100,7 @@ public class UpdateHandler(
     private readonly List<UserJob> _failedJobsRepository = [];
     private const int MaxChunkSize = 30;
     private const string PlaylistStartUrl = "https://music.youtube.com/playlist";
-    private const string SuccessStatus = "Succeeded";
-    private const string FailedStatus = "Failed";
+
     private const string RestartCommand = "/restart";
     private static readonly TimeSpan WaitInterval = TimeSpan.FromSeconds(20);
     private static readonly TimeSpan MaxWaitTime = TimeSpan.FromMinutes(15);
@@ -82,85 +150,67 @@ public class UpdateHandler(
     {
         var waitingMessage = await bot.SendTextMessageAsync(msg.Chat, "\u23f3");
 
+        UserJob? userJob = null;
         try
         {
-            var msgText = msg.Text;
-            if (msgText == null)
+            var text = msg.Text;
+            if (text == null)
             {
                 return await bot.SendTextMessageAsync(msg.Chat, "Message text is null",
                     replyParameters: new ReplyParameters { MessageId = msg.MessageId });
             }
 
-            var jobId = msgText.Split(' ')[1];
-            if (string.IsNullOrEmpty(jobId))
+            var id = text.Split(' ')[1];
+            if (string.IsNullOrEmpty(id))
             {
                 return await bot.SendTextMessageAsync(msg.Chat, "Job id is empty",
                     replyParameters: new ReplyParameters { MessageId = msg.MessageId });
             }
 
-            var userFailedJobs = _failedJobsRepository
-                .Where(userJob => userJob.UserId.Equals(msg.Chat.Id)).ToArray();
+            var jobs = _failedJobsRepository
+                .Where(job => job.UserId.Equals(msg.Chat.Id)).ToArray();
             
-            if (userFailedJobs.Length == 0)
+            if (jobs.Length == 0)
             {
                 return await bot.SendTextMessageAsync(msg.Chat, "You do not have failed jobs.",
                     replyParameters: new ReplyParameters { MessageId = msg.MessageId });
             }
 
-            var userFailedJob = userFailedJobs.FirstOrDefault(userJob => userJob.Job.JobId.Equals(jobId));
-            if (userFailedJob == null)
+            userJob = jobs.FirstOrDefault(job => job.Job.JobId.Equals(id));
+            if (userJob == null)
             {
-                return await bot.SendTextMessageAsync(msg.Chat, $"You do not have a failed job with id '{jobId}'.",
+                return await bot.SendTextMessageAsync(msg.Chat, $"You do not have a failed job with id '{id}'.",
                     replyParameters: new ReplyParameters { MessageId = msg.MessageId });
             }
+
+            var jb = userJob.Job;
             
-            await ytMusicApi.CheckHealth();
-            logger.LogInformation("The music service is ready.");
+            await bot.SendTextMessageAsync(msg.Chat, $"The job with info will be restarted:\n\n{await GetJobTextDescription(jb)}",
+                replyParameters: new ReplyParameters { MessageId = msg.MessageId });
             
-            logger.LogInformation("Attempting to retrieve auth data");
-
-            var data = new Dictionary<string, string> {
-                { IAuthApi.GrantType, IAuthApi.ClientCredentials },
-                { IAuthApi.ClientId, configuration["BotConfiguration:ClientId"] ?? "" },
-                { IAuthApi.ClientSecret, configuration["BotConfiguration:ClientSecret"] ?? "" }
-            };
-
-            var authData = await authApi.GetAuthData(data);
-            logger.LogInformation("Auth data retrieved successfully");
-
-            var makingArchiveJob = await ytMusicApi.StartMakingArchiveJob($"Bearer {authData.AccessToken}",
-                new UrlsRequest { Urls = userFailedJob.Job.Urls });
+            await jb.IgniteRemoteJob();
             
             var startTime = DateTime.UtcNow;
-
-            YtMusicStatusResponseData? statusResponseData = null;
+            
             while (DateTime.UtcNow - startTime < MaxWaitTime)
             {
-                statusResponseData = await ytMusicApi.CheckStatus(makingArchiveJob.JobId);
+                await jb.UpdateStatus();
                 
-                if (statusResponseData.Status is SuccessStatus or FailedStatus)
+                if (jb.Status is Statuses.SuccessStatus or Statuses.FailedStatus)
                 {
                     break;
                 }
-
+                
                 await Task.Delay(WaitInterval);
             }
             
-            if (statusResponseData == null)
+            if (jb.Status is not Statuses.SuccessStatus)
             {
                 return await bot.SendTextMessageAsync(msg.Chat, "Unable to do the job.",
                     replyParameters: new ReplyParameters { MessageId = msg.MessageId });
             }
             
-            if (statusResponseData.Status != SuccessStatus)
-            {
-                return await bot.SendTextMessageAsync(msg.Chat, "Unable to do the job.",
-                    replyParameters: new ReplyParameters { MessageId = msg.MessageId });
-            }
-            
-            _failedJobsRepository.Remove(userFailedJob);
-            
-            return await bot.SendTextMessageAsync(msg.Chat, $"This is your direct link: {statusResponseData.Result}",
+            return await bot.SendTextMessageAsync(msg.Chat, $"This is your direct link: {jb.Result}",
                 replyParameters: new ReplyParameters { MessageId = msg.MessageId });
         }
         catch (Exception ex)
@@ -170,12 +220,15 @@ public class UpdateHandler(
         }
         finally
         {
+            if (userJob != null)
+            {
+                _failedJobsRepository.Remove(userJob);    
+            }
+            
             await bot.DeleteMessageAsync(msg.Chat, waitingMessage.MessageId);
         }
     }
-
-    public record Job(string JobId, string[] Urls);
-
+    
     private async Task<Message> DefaultBehaviour(Message msg)
     {
         var waitingMessage = await bot.SendTextMessageAsync(msg.Chat, "\u23f3");
@@ -212,41 +265,38 @@ public class UpdateHandler(
                 return await bot.SendTextMessageAsync(msg.Chat, "Playlist is empty",
                     replyParameters: new ReplyParameters { MessageId = msg.MessageId });
             }
-            
-            await bot.SendTextMessageAsync(msg.Chat, $"The whole playlist contains {tracks.Count} tracks. I will split the playlist into multiple ({allChunks.Length}) chunks. Each chunk will contain maximum {MaxChunkSize} tracks.",
-                replyParameters: new ReplyParameters { MessageId = msg.MessageId });
-            
-            await ytMusicApi.CheckHealth();
-            logger.LogInformation("The music service is ready.");
-            
-            logger.LogInformation("Attempting to retrieve auth data");
-
-            var data = new Dictionary<string, string> {
-                { IAuthApi.GrantType, IAuthApi.ClientCredentials },
-                { IAuthApi.ClientId, configuration["BotConfiguration:ClientId"] ?? "" },
-                { IAuthApi.ClientSecret, configuration["BotConfiguration:ClientSecret"] ?? "" }
-            };
-
-            var authData = await authApi.GetAuthData(data);
-            logger.LogInformation("Auth data retrieved successfully");
 
             var jobs = new List<Job>();
-            
             foreach (var chunk in allChunks)
             {
-                if (authData.ShouldRefreshToken(refreshThresholdSeconds: 180))
-                {
-                    authData = await authApi.GetAuthData(data);
-                }
-
-                var urls = chunk.Select(video => video.Url).ToArray();
-                var job = await ytMusicApi.StartMakingArchiveJob($"Bearer {authData.AccessToken}",
-                    new UrlsRequest { Urls = urls});
-
-                jobs.Add(new Job(job.JobId, urls));
-
-                await Task.Delay(TimeSpan.FromMinutes(1));
+                var job = new Job(chunk.Select(video => video.Url).ToArray(), ytMusicApi, authApi, configuration);
+                jobs.Add(job);
             }
+
+            var sb = new StringBuilder();
+            
+            foreach (var job in jobs)
+            {
+                sb.AppendLine(await GetJobTextDescription(job));
+            }
+            
+            await bot.SendTextMessageAsync(
+                chatId: msg.Chat,
+                text: $"""
+                       You will receive next chunks:
+                       
+                       {sb}
+                       """,
+                replyParameters: new ReplyParameters { MessageId = msg.MessageId }
+            );
+            
+            foreach (var job in jobs)
+            {
+                await job.IgniteRemoteJob();
+                await Task.Delay(WaitInterval);
+            }
+            
+            logger.LogInformation("All jobs started...");
             
             foreach (var job in jobs)
             {
@@ -254,17 +304,16 @@ public class UpdateHandler(
             }
             
             var startTime = DateTime.UtcNow;
-            (string id, string Status, string Result)[] jobStatuses = [];
 
             while (DateTime.UtcNow - startTime < MaxWaitTime)
             {
-                jobStatuses = await Task.WhenAll(jobs.Select(async job =>
+                foreach (var job in jobs)
                 {
-                    var status = await ytMusicApi.CheckStatus(job.JobId);
-                    return (id: job.JobId, status.Status, status.Result);
-                }));
+                    await job.UpdateStatus();
+                    await Task.Delay(TimeSpan.FromSeconds(1));
+                }
 
-                if (jobStatuses.All(js => js.Status is SuccessStatus or FailedStatus))
+                if (jobs.All(job => job.Status is Statuses.SuccessStatus or Statuses.FailedStatus))
                 {
                     break;
                 }
@@ -272,37 +321,28 @@ public class UpdateHandler(
                 await Task.Delay(WaitInterval);
             }
 
-            if (jobStatuses.Any(js => js.Status != SuccessStatus))
-            {
-                var successJobs = jobStatuses.Where(js => js.Status == SuccessStatus).ToArray();
+            var successJobs = jobs.Where(job =>
+                job.Status == Statuses.SuccessStatus && string.IsNullOrWhiteSpace(job.Result) == false).ToArray();
                 
-                for (var i = 0; i < successJobs.Length; i++)
+            await SentHtmlMsg(msg, successJobs);
+            
+            if (jobs.Any(js => js.Status is not Statuses.SuccessStatus))
+            {
+                var failedJobs = jobs.Where(js => js.Status is Statuses.FailedStatus or Statuses.UnknownStatus).ToArray();
+                foreach (var job in failedJobs)
                 {
-                    await SendHtmlResult(msg, successJobs, i);
-                }
-
-                var failedJobs = jobStatuses.Where(js => js.Status == FailedStatus).ToArray();
-                foreach ((string id, string Status, string Result) tuple in failedJobs)
-                {
-                    var foundFailedJob = jobs.FirstOrDefault(job => job.JobId.Equals(tuple.id));
-                    if (foundFailedJob != null)
-                        _failedJobsRepository.Add(new UserJob(msg.Chat.Id, foundFailedJob));
+                    _failedJobsRepository.Add(new UserJob(msg.Chat.Id, job));
                 }
 
                 var builder = new StringBuilder();
-                foreach (var (id, _, _) in failedJobs)
+                foreach (var job in failedJobs)
                 {
-                    builder.AppendLine($"`/restart {id}`");
+                    builder.AppendLine($"`/restart {job.JobId}`");
                 }
 
                 return await bot.SendTextMessageAsync(msg.Chat, $"Some jobs did not succeed. You can restart failed jobs:\n{builder}",
                     parseMode: ParseMode.Markdown,
                     replyParameters: new ReplyParameters { MessageId = msg.MessageId });
-            }
-            
-            for (var i = 0; i < jobStatuses.Length; i++)
-            {
-                await SendHtmlResult(msg, jobStatuses, i);
             }
             
             return await bot.SendTextMessageAsync(msg.Chat, "These are your direct links \ud83d\udc46",
@@ -319,15 +359,26 @@ public class UpdateHandler(
         }
     }
 
-    private async Task SendHtmlResult(Message msg, (string id, string Status, string Result)[] jobStatuses, int i)
+    private static async Task<string> GetJobTextDescription(Job job)
     {
-        var (_, _, result) = jobStatuses[i];
-        logger.LogInformation("Job result: {Result}", result);
-        await bot.SendTextMessageAsync(msg.Chat, $"<a href=\"{result}\">Part {i + 1}</a>",
-            parseMode: ParseMode.Html,
-            replyParameters: new ReplyParameters { MessageId = msg.MessageId });
+        return $"""
+                1. {await job.GetFirstTrackName()}
+                ...
+                n. {await job.GetLastTrackName()}
+                _____________________
+                """;
+    }
 
-        await Task.Delay(TimeSpan.FromSeconds(1));
+    private async Task SentHtmlMsg(Message msg, Job[] jobs)
+    {
+        for (var i = 0; i < jobs.Length; i++)
+        {
+            await bot.SendTextMessageAsync(msg.Chat, $"<a href=\"{jobs[i].Result}\">Part {i + 1}</a>",
+                parseMode: ParseMode.Html,
+                replyParameters: new ReplyParameters { MessageId = msg.MessageId });
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
     }
 
     #region Inline Mode
